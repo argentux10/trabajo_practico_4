b1465ca92f30bfd366b3ffc6d816255d24e219d0
Ezequiel-PC <Ezequiel-PC@DESKTOP-SELTJ5M>
2024-05-26 08:38:17 -0300

    Se agrego al archivo pom.xml las dependencias webjars y bootstrap

---
+++ tp4_JaljalEzequiel/src/main/java/com/example/demo/Tp4JaljalEzequielApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4JaljalEzequielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4JaljalEzequielApplication.class, args);
+	}
+
+}
